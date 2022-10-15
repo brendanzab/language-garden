a9b9a87023e4455924f2efffd632353cca0a330a
brendanzab <bjzaba@yahoo.com.au>
2022-10-15 12:34:33 +1100

    compile-arith: Avoid list concatenation

--- compile-arith/lib/TreeToStack.ml
+++ compile-arith/lib/TreeToStack.ml
@@ -1,11 +1,22 @@
 type source = TreeLang.expr
 type target = StackLang.code
 
-let rec translate : TreeLang.expr -> StackLang.code =
+
+(** Function composition *)
+let (<<) f g x = f (g x)
+
+(** Translate an expression, adding it to the continuation of the expression.
+    The continuation allows us to avoid using list concatenation, which could
+    lead to exponential blowups during compilation. *)
+let rec translate_code : TreeLang.expr -> StackLang.code -> StackLang.code =
   function
-  | TreeLang.Int i -> [StackLang.Int i]
-  | TreeLang.Neg e -> translate e @ [StackLang.Neg]
-  | TreeLang.Add (e1, e2) -> translate e1 @ translate e2 @ [StackLang.Add]
-  | TreeLang.Sub (e1, e2) -> translate e1 @ translate e2 @ [StackLang.Sub]
-  | TreeLang.Mul (e1, e2) -> translate e1 @ translate e2 @ [StackLang.Mul]
-  | TreeLang.Div (e1, e2) -> translate e1 @ translate e2 @ [StackLang.Div]
+  | TreeLang.Int i -> List.cons (StackLang.Int i)
+  | TreeLang.Neg e -> translate_code e << List.cons StackLang.Neg
+  | TreeLang.Add (e1, e2) -> translate_code e1 << translate_code e2 << List.cons StackLang.Add
+  | TreeLang.Sub (e1, e2) -> translate_code e1 << translate_code e2 << List.cons StackLang.Sub
+  | TreeLang.Mul (e1, e2) -> translate_code e1 << translate_code e2 << List.cons StackLang.Mul
+  | TreeLang.Div (e1, e2) -> translate_code e1 << translate_code e2 << List.cons StackLang.Div
+
+
+let translate (e : TreeLang.expr) : StackLang.code =
+  translate_code e []
--- compile-arithcond/lib/TreeToStack.ml
+++ compile-arithcond/lib/TreeToStack.ml
@@ -1,18 +1,35 @@
 type source = TreeLang.expr
 type target = StackLang.code
 
-let rec translate : TreeLang.expr -> StackLang.code =
+
+(** Function composition *)
+let (<<) f g x = f (g x)
+
+(** Translate an expression, adding it to the continuation of the expression.
+    The continuation allows us to avoid using list concatenation, which could
+    lead to exponential blowups during compilation. *)
+let rec translate_code : TreeLang.expr -> StackLang.code -> StackLang.code =
   function
-  | TreeLang.Var n -> [StackLang.Access n]
+  | TreeLang.Var n -> List.cons (StackLang.Access n)
   | TreeLang.Let (_, e1, e2) ->
-      translate e1 @ [StackLang.BeginLet] @ translate e2 @ [StackLang.EndLet]
-  | TreeLang.Int i -> [StackLang.Int i]
-  | TreeLang.Bool b -> [StackLang.Bool b]
-  | TreeLang.Neg e -> translate e @ [StackLang.Neg]
-  | TreeLang.Add (e1, e2) -> translate e1 @ translate e2 @ [StackLang.Add]
-  | TreeLang.Sub (e1, e2) -> translate e1 @ translate e2 @ [StackLang.Sub]
-  | TreeLang.Mul (e1, e2) -> translate e1 @ translate e2 @ [StackLang.Mul]
-  | TreeLang.Div (e1, e2) -> translate e1 @ translate e2 @ [StackLang.Div]
-  | TreeLang.Eq (e1, e2) -> translate e1 @ translate e2 @ [StackLang.Eq]
+      translate_code e1
+        << List.cons StackLang.BeginLet
+        << translate_code e2
+        << List.cons StackLang.EndLet
+  | TreeLang.Int i -> List.cons (StackLang.Int i)
+  | TreeLang.Bool b -> List.cons (StackLang.Bool b)
+  | TreeLang.Neg e -> translate_code e << List.cons StackLang.Neg
+  | TreeLang.Add (e1, e2) -> translate_code e1 << translate_code e2 << List.cons StackLang.Add
+  | TreeLang.Sub (e1, e2) -> translate_code e1 << translate_code e2 << List.cons StackLang.Sub
+  | TreeLang.Mul (e1, e2) -> translate_code e1 << translate_code e2 << List.cons StackLang.Mul
+  | TreeLang.Div (e1, e2) -> translate_code e1 << translate_code e2 << List.cons StackLang.Div
+  | TreeLang.Eq (e1, e2) -> translate_code e1 << translate_code e2 << List.cons StackLang.Eq
   | TreeLang.IfThenElse(e1, e2, e3) ->
-      translate e1 @ [StackLang.Code (translate e2); StackLang.Code (translate e3); StackLang.IfThenElse]
+      translate_code e1
+        << List.cons (StackLang.Code (translate_code e2 []))
+        << List.cons (StackLang.Code (translate_code e3 []))
+        << List.cons StackLang.IfThenElse
+
+
+let translate (e : TreeLang.expr) : StackLang.code =
+  translate_code e []
