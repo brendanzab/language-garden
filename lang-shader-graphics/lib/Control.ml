eb7c556ba1fe5c95cea9a6bd98efd1e16a947cd8
brendanzab <bjzaba@yahoo.com.au>
2022-10-23 17:54:01 +1100

    lang-shader-graphics: Add more operators

--- lang-shader-graphics/lib/Control.ml
+++ lang-shader-graphics/lib/Control.ml
@@ -1,10 +1,35 @@
 module Functor = struct
 
+  (** A type constructor ['a t] that supports a mapping operation [map]. *)
   module type S = sig
+    (** This is technically an “endofunctor” between the ‘category’ of OCaml
+        types and functions. *)
 
     type 'a t
 
-    val map : ('a -> 'b) -> 'a t -> 'b t
+    (** Turn a function of type ['a -> 'b] into a function of type
+        ['a t -> 'b t]. *)
+    val map : ('a -> 'b) -> ('a t -> 'b t)
+
+    (** {1 Laws}
+
+        - Identity: [map Fun.id = Fun.id]
+        - Composition: [map (f >> g) = map f >> map g]
+    *)
+
+  end
+
+
+  (** Utility functions derived from functors *)
+  module Util (F : S) = struct
+
+    open F
+
+    let void_left : 'a -> 'b t -> 'a t =
+      fun x f -> map (Fun.const x) f
+
+    let void_right : 'b t -> 'a -> 'a t =
+      fun f x -> map (Fun.const x) f
 
   end
 
@@ -15,19 +40,106 @@ module Functor = struct
     type 'a t
 
     val ( let+ ) : 'a t -> ('a -> 'b) -> 'b t
+    val ( >|= ) : 'a t -> ('a -> 'b) -> 'b t
+    val ( <$> ) : ('a -> 'b) -> 'a t -> 'b t
+    val ( <$ ) : 'a -> 'b t -> 'a t
+    val ( $> ) : 'b t -> 'a -> 'a t
 
   end
 
 
+  (** Construct some notation for a functor *)
   module Notation (F : S) : Notation
 
     with type 'a t = 'a F.t
 
   = struct
 
+    open F
+    open Util (F)
+
     type 'a t = 'a F.t
 
-    let ( let+ ) t f = F.map f t
+    let ( let+ ) t f = map f t
+    let ( >|= ) t f = map f t
+    let ( <$> ) = map
+    let ( <$ ) = void_left
+    let ( $> ) = void_right
+
+  end
+
+
+  (** The composition of two functors *)
+  module Compose (F : S) (G : S) : S
+
+    with type 'a t = 'a G.t F.t
+
+  = struct
+
+    type 'a t = 'a G.t F.t
+
+    let map f x = F.map (G.map f) x
+
+  end
+
+end
+
+
+(** Applicative functors *)
+module Applicative = struct
+
+  module type S = sig
+
+    type 'a t
+
+    include Functor.S with type 'a t := 'a t
+
+    (** Embed a pure value in [t] *)
+    val pure : 'a -> 'a t
+
+    (** Apply a function embedded in [t] to a value embedded in [t] *)
+    val apply : ('a -> 'b) t -> 'a t -> 'b t
+
+  end
+
+
+  (** Operators to make working with applicatives more pleasant *)
+  module type Notation = sig
+
+    type 'a t
+
+    include Functor.Notation with type 'a t := 'a t
+
+    val ( <*> ) : ('a -> 'b) t -> 'a t -> 'b t
+
+  end
+
+
+  (** Construct some notation for an applicative *)
+  module Notation (A : S) : Notation
+
+    with type 'a t = 'a A.t
+
+  = struct
+
+    include Functor.Notation (A)
+
+    let ( <*> ) = A.apply
+
+  end
+
+
+  (** The composition of two applicatives *)
+  module Compose (F : S) (G : S) : S
+
+    with type 'a t = 'a G.t F.t
+
+  = struct
+
+    include Functor.Compose (F) (G)
+
+    let pure x = F.pure (G.pure x)
+    let apply f x = F.apply (F.map G.apply f) x
 
   end
 
@@ -40,9 +152,8 @@ module Monad = struct
 
     type 'a t
 
-    include Functor.S with type 'a t := 'a t
+    include Applicative.S with type 'a t := 'a t
 
-    val pure : 'a -> 'a t
     val bind : 'a t -> ('a -> 'b t) -> 'b t
 
   end
@@ -53,7 +164,7 @@ module Monad = struct
 
     type 'a t
 
-    include Functor.Notation with type 'a t := 'a t
+    include Applicative.Notation with type 'a t := 'a t
 
     val ( let* ) : 'a t -> ('a -> 'b t) -> 'b t
     val ( and* ) : 'a t -> 'b t -> ('a * 'b) t
@@ -62,13 +173,14 @@ module Monad = struct
   end
 
 
+  (** Construct some notation for a monad *)
   module Notation (M : S) : Notation
 
     with type 'a t = 'a M.t
 
   = struct
 
-    include Functor.Notation (M)
+    include Applicative.Notation (M)
 
     let ( let* ) = M.bind
 
@@ -140,8 +252,9 @@ module Monad = struct
     type 'a t = value -> 'a
 
     let map f x = fun v ->  f (x v)
-    let bind t f = fun v -> f (t v) v
     let pure v = fun _ -> v
+    let apply f x = fun v -> (f v) (x v)
+    let bind t f = fun v -> f (t v) v
 
     let read = fun v -> v
     let scope f t = fun v -> t (f v)
@@ -195,11 +308,17 @@ module Monad = struct
       let x, s = x s in
       f x, s
 
-    let bind x f = fun s ->
-      let x', s = x s in
-      f x' s
+    let pure x =
+      fun s -> x, s
 
-    let pure x = fun s -> x, s
+    let apply f x = fun s ->
+      let f, s = f s in
+      let x, s = x s in
+      f x, s
+
+    let bind x f = fun s ->
+      let x, s = x s in
+      f x s
 
     let get = fun s -> s, s
     let put s = fun _ -> (), s
@@ -213,5 +332,22 @@ module Monad = struct
   (** A state monad with an abstract implementation *)
   module State (S : sig type t end) : State with type state = S.t =
     FunctionState (S)
+
+end
+
+
+(** Operations on the output (returned value) of a function *)
+module Output (Input : sig type t end) : Monad.S
+
+  with type 'a t = Input.t -> 'a
+
+= struct
+
+  type 'a t = Input.t -> 'a
+
+  let map f g = fun x -> f (g x)
+  let pure x = fun _ -> x
+  let apply f g = fun x -> (f x) (g x)
+  let bind f g = fun x -> g (f x) x
 
 end
