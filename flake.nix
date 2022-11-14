ea431020d106bb3cd64a57d8f154a1fe6d9da15f
brendanzab <bjzaba@yahoo.com.au>
2022-11-14 14:30:39 +1100

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'flake-utils':
        'github:numtide/flake-utils/c0e246b9b83f637f4681389ecabcb2681b4f3af0' (2022-08-07)
      → 'github:numtide/flake-utils/5aed5285a952e0b949eb3ba02c12fa4fcfef535f' (2022-11-02)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/4dd035239719fc7992a4214934ecaf2a943d5ec0' (2022-09-27)
      → 'github:NixOS/nixpkgs/5ca8e2e9e1fa5e66a749b39261ad6bd0e07bc87f' (2022-11-12)

--- flake.nix
+++ flake.nix
@@ -13,10 +13,7 @@
   #
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
