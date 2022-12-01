43d19604dcee28a35d9b0192a60e32a32b391dbe
brendanzab <bjzaba@yahoo.com.au>
2022-12-01 19:31:52 +1100

    wip-compile-stratify: Qualify rule module

--- wip-compile-stratify/Main.ml
+++ wip-compile-stratify/Main.ml
@@ -1,68 +1,68 @@
 let () =
-  let open CoreRules in
+  let module R = CoreRules in
 
   Printexc.record_backtrace true;
 
   (* TODO: Pretty printing *)
 
-  let _, tm = run_synth @@ Univ.univ L0 in
+  let _, tm = R.run_synth @@ R.Univ.univ L0 in
   let _ = Core.Validation.(synth Context.empty tm) in
 
-  let _, tm = run_synth @@ Univ.fun_ (Univ.univ L0) @@ fun u -> u in
+  let _, tm = R.run_synth @@ R.Univ.fun_ (R.Univ.univ L0) @@ fun u -> u in
   let _ = Core.Validation.(synth Context.empty tm) in
 
-  let _, tm = run_synth @@ Structure.let_synth (Univ.univ L0) @@ fun u -> u in
+  let _, tm = R.run_synth @@ R.Structure.let_synth (R.Univ.univ L0) @@ fun u -> u in
   let _ = Core.Validation.(synth Context.empty tm) in
 
 
   (* Identity function (synthesis) *)
 
   let id =
-    Fun.intro_synth ~name:"A" (is_ty @@ Univ.univ L0) @@ fun a ->
-    Fun.intro_synth ~name:"x" (is_ty a) @@ fun x -> x
+    R.Fun.intro_synth ~name:"A" (R.is_ty @@ R.Univ.univ L0) @@ fun a ->
+    R.Fun.intro_synth ~name:"x" (R.is_ty a) @@ fun x -> x
   in
 
-  let _, tm = run_synth id in
+  let _, tm = R.run_synth id in
   let _ = Core.Validation.(synth Context.empty tm) in
 
   let app =
-    Structure.let_synth ~name:"id" id @@ fun id ->
-    Fun.intro_synth ~name:"B" (is_ty @@ Univ.univ L0) @@ fun b ->
-      Fun.app id (check b)
+    R.Structure.let_synth ~name:"id" id @@ fun id ->
+    R.Fun.intro_synth ~name:"B" (R.is_ty @@ R.Univ.univ L0) @@ fun b ->
+      R.Fun.app id (R.check b)
   in
 
-  let _, tm = run_synth app in
+  let _, tm = R.run_synth app in
   let _ = Core.Validation.(synth Context.empty tm) in
 
 
   (* Identity function (checking) *)
 
   let id_ty =
-    Univ.fun_ (Univ.univ L0) @@ fun a ->
-    Univ.fun_ a @@ fun _ -> a
+    R.Univ.fun_ (R.Univ.univ L0) @@ fun a ->
+    R.Univ.fun_ a @@ fun _ -> a
   in
   let id =
-    ann ~ty:(is_ty id_ty) @@
-      Fun.intro_check ~name:"A" @@ fun _ ->
-      Fun.intro_check ~name:"x" @@ fun x ->
-        check x
+    R.ann ~ty:(R.is_ty id_ty) @@
+      R.Fun.intro_check ~name:"A" @@ fun _ ->
+      R.Fun.intro_check ~name:"x" @@ fun x ->
+        R.check x
   in
 
-  let _, tm = run_synth id in
+  let _, tm = R.run_synth id in
   let _ = Core.Validation.(synth Context.empty tm) in
 
 
   let app_ty =
-    Univ.fun_ (Univ.univ L0) @@ fun b ->
-    Univ.fun_ b @@ fun _ -> b
+    R.Univ.fun_ (R.Univ.univ L0) @@ fun b ->
+    R.Univ.fun_ b @@ fun _ -> b
   in
   let app =
-    ann ~ty:(is_ty app_ty) @@
-      Fun.intro_check ~name:"B" @@ fun b ->
-        check (Fun.app id (check b))
+    R.ann ~ty:(R.is_ty app_ty) @@
+      R.Fun.intro_check ~name:"B" @@ fun b ->
+        R.check (R.Fun.app id (R.check b))
   in
 
-  let _, tm = run_synth app in
+  let _, tm = R.run_synth app in
   let _ = Core.Validation.(synth Context.empty tm) in
 
   ()
