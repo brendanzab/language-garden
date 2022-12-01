84cf622f63037dcad7ec27972119cc036a9ca6a6
brendanzab <bjzaba@yahoo.com.au>
2022-12-01 21:10:50 +1100

    wip-compile-stratify: Improve doc comments

--- wip-compile-stratify/CoreRules.mli
+++ wip-compile-stratify/CoreRules.mli
@@ -2,12 +2,24 @@
 
 (** Trusted typing rules of the core language.
 
+    This module exposes a clean, high-level signature of the inference rules of
+    the core language. An elaborator can then dispatch to this API, avoiding the
+    need to manipulate the innards of core terms and values directly.
+
+    The idea for this module was inspired by {{:https://github.com/jonsterling/dreamtt/}
+    DreamTT} and {{:https://github.com/RedPRL/cooltt/} CoolTT}, but apparently
+    it dates back to “The LCF Approach” (pointers to a reference for this would
+    be most appreciated).
+
    {1 References}
 
     - {{:https://bentnib.org/posts/2015-04-19-algebraic-approach-typechecking-and-elaboration.html}
       An Algebraic Approach to Typechecking and Elaboration} by Robert Atkey
+    - {{:https://github.com/jonsterling/dreamtt/} DreamTT: A pedagogic
+      implementation of abstract bidirectional elaboration for dependent type
+      theory} by Jon Sterling
     - {{:https://gist.github.com/TOTBWF/9b2c071d2edb1c6596b785656c866fd6}
-       MicroTT.ml:  A simple single-file elaborator for MLTT} by Reed Mullanix
+       MicroTT.ml: A simple single-file elaborator for MLTT} by Reed Mullanix
 *)
 
 (** {1 Exceptions} *)
@@ -19,8 +31,13 @@ exception Error of string
 
 (** {1 Rule types} *)
 
+(** A rule that yields a type and the level of universe it inhabits *)
 type is_ty
+
+(** A ‘synthesis’ rule that yields a term and its synthesised type *)
 type synth
+
+(** A ‘checkable’ rule that yeilds a term when a type annotation is supplied *)
 type check
 
 (** {2 Running rules} *)
@@ -43,8 +60,8 @@ val ann : ty:is_ty -> check -> synth
 (** Turn a synthesis rule into a type rule. *)
 val is_ty : synth -> is_ty
 
-(** Turn a synth rule into a check rule by checking that the synthesised type
-    matches the expected type. *)
+(** Turn a synthesis rule into a checkable rule by checking that the synthesised
+    type matches the expected type. *)
 val check : synth -> check
 
 
@@ -67,10 +84,10 @@ end
 (** Rules related to functions *)
 module Fun : sig
 
-  (** Function introduction *)
+  (** Function introduction (in synthesis mode) *)
   val intro_synth : ?name:string -> ty:is_ty -> (synth -> synth) -> synth
 
-  (** Function introduction *)
+  (** Function introduction (in checking mode) *)
   val intro_check : ?name:string -> ?ty:is_ty -> (synth -> check) -> check
 
   (** Function elimination *)
