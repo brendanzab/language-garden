f835e6102d79a23cde6fbc7737414e220195c293
brendanzab <bjzaba@yahoo.com.au>
2023-03-03 01:10:52 +1100

    elab-stlc-unification: Fix meta printing

--- elab-stlc-unification/Core.ml
+++ elab-stlc-unification/Core.ml
@@ -160,13 +160,12 @@ let rec pp_ty fmt ty =
 and pp_atomic_ty fmt =
   function
   | IntType -> Format.fprintf fmt "Int"
-  | MetaVar m -> pp_meta fmt m
+  | MetaVar m ->
+      begin match !m with
+      | Solved ty -> pp_atomic_ty fmt ty
+      | Unsolved id -> Format.fprintf fmt "?%i" id
+      end
   | ty -> Format.fprintf fmt "@[(%a)@]" pp_ty ty
-
-and pp_meta fmt m =
-  match !m with
-  | Solved ty -> pp_ty fmt ty
-  | Unsolved id -> Format.fprintf fmt "?%i" id
 
 let pp_name_ann fmt (name, ty) =
   Format.fprintf fmt "@[<2>@[%s :@]@ %a@]" name pp_ty ty
