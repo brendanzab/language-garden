97f9f2d460a5894b4b2adf0a1612aec902b7d4e0
brendanzab <bjzaba@yahoo.com.au>
2023-09-10 19:11:13 +1000

    flake.nix: Remove flake-utils
    This adds a `systems` input that can be overridden by consumers to
    adjust the systems supported by the flake. For more information see:
    <https://github.com/nix-systems/nix-systems>.

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Programming language garden";
+  description = "A garden of toy programming language implementations";
 
   # Flake dependency specification
   #
@@ -13,13 +13,23 @@
   #
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
 
+    # Externally extensible flake systems. See <https://github.com/nix-systems/nix-systems>.
+    systems.url = "github:nix-systems/default";
+
+    # Follow the `systems` input in flakes that still use `flake-utils`.
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+
+    # Main package repository for opam, the package manager of OCaml.
     opam-repository = {
       url = "github:ocaml/opam-repository";
       flake = false;
     };
 
+    # Create nix derivations from opam package definitions.
     opam-nix = {
       url = "github:tweag/opam-nix";
       inputs.flake-utils.follows = "flake-utils";
@@ -28,93 +38,124 @@
     };
   };
 
-  outputs = { self, flake-utils, opam-nix, nixpkgs, ... }@inputs:
-    # Construct an output set that supports a number of default systems
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        opam-lib = opam-nix.lib.${system};
+  outputs = { self, systems, opam-nix, nixpkgs, ... }@inputs:
+    let
+      # Iterate over each system, configured via the `systems` input.
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
 
-        # Local packages, detected from the `*.opam` files found in `./opam/`.
-        localPackagesQuery = builtins.mapAttrs (_: pkgs.lib.last)
-          (opam-lib.listRepo (opam-lib.makeOpamRepo ./.));
-        # Development packages, automatically added to the development shell.
-        devPackagesQuery = {
-          ocaml-lsp-server = "*";
-          # FIXME: ocamlformat 0.26.0 fails to build with:
-          #
-          #     File "lib/dune", line 39, characters 2-16:
-          #     39 |   ocp-indent.lib
-          #            ^^^^^^^^^^^^^^
-          #     Error: Library "ocp-indent.lib" not found.
-          #     -> required by library "ocamlformat-lib" in _build/default/lib
-          #     -> required by _build/default/META.ocamlformat-lib
-          #     -> required by _build/install/default/lib/ocamlformat-lib/META
-          #     -> required by _build/default/ocamlformat-lib.install
-          #     -> required by alias install
-          ocamlformat = "0.25.1";
-          utop = "*";
-        };
-        query = devPackagesQuery // {
-          ## Force versions of certain packages here, e.g:
-          ## - force the ocaml compiler to be taken from opam-repository:
-          ocaml-base-compiler = "5.0.0"; # Fix for opam-nix choosing 5.0.0~rc1 over 5.0.0
-          ## - or force the compiler to be taken from nixpkgs and be a certain version:
-          # ocaml-system = "4.14.0";
-          ## - or force ocamlfind to be a certain version:
-          # ocamlfind = "1.9.2";
-        };
+      # Local packages, detected from the package definition files in `./opam/`.
+      localPackagesQuery = eachSystem (system:
+        let
+          opam-lib = opam-nix.lib.${system};
+        in
+        nixpkgs.lib.mapAttrs (_: nixpkgs.lib.last)
+          (opam-lib.listRepo (opam-lib.makeOpamRepo ./.)));
 
-        overlay = final: prev: {
+      # Development package versions.
+      devPackagesQuery = {
+        ocaml-lsp-server = "*";
+        # FIXME: ocamlformat 0.26.0 fails to build with:
+        #
+        #     File "lib/dune", line 39, characters 2-16:
+        #     39 |   ocp-indent.lib
+        #            ^^^^^^^^^^^^^^
+        #     Error: Library "ocp-indent.lib" not found.
+        #     -> required by library "ocamlformat-lib" in _build/default/lib
+        #     -> required by _build/default/META.ocamlformat-lib
+        #     -> required by _build/install/default/lib/ocamlformat-lib/META
+        #     -> required by _build/default/ocamlformat-lib.install
+        #     -> required by alias install
+        ocamlformat = "0.25.1";
+        utop = "*";
+      };
+
+      # Development package versions, along with the base compiler tools, used
+      # when building the opam project with `opam-nix`.
+      query = devPackagesQuery // {
+        # Force the ocaml compiler to be taken from opam-repository:
+        ocaml-base-compiler = "5.0.0"; # Fix for opam-nix choosing 5.0.0~rc1 over 5.0.0
+      };
+
+      # Package-specific derivation overrides.
+      overlay = eachSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        final: prev: {
           lang-shader-graphics = prev.lang-shader-graphics.overrideAttrs (previousAttrs: {
             nativeCheckInputs = [
               # For `lang-shader-graphics/test/dune`
               pkgs.netpbm
             ];
           });
-        };
+        });
 
-        buildOpamProject = options:
-          (opam-lib.buildOpamProject' options ./. query).overrideScope' overlay;
+      buildOpamProject = system: options:
+        (opam-nix.lib.${system}.buildOpamProject' options ./. query).overrideScope'
+          overlay.${system};
 
-        legacyPackages = buildOpamProject { };
-        devPackages = pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) legacyPackages;
-        packages = pkgs.lib.getAttrs (builtins.attrNames localPackagesQuery) legacyPackages;
+      legacyPackages = eachSystem (system:
+        buildOpamProject system { });
 
-        legacyTestPackages = buildOpamProject { resolveArgs.with-test = true; };
-        testPackages = pkgs.lib.getAttrs (builtins.attrNames localPackagesQuery) legacyTestPackages;
-      in
-      {
-        # Exposed packages that can be built or run with `nix build` or
-        # `nix run` respectively:
-        #
-        #     $ nix build .#<name>
-        #     $ nix run .#<name> -- <args?>
-        #
-        inherit packages;
+      packages = eachSystem (system:
+        nixpkgs.lib.getAttrs
+          (nixpkgs.lib.attrNames localPackagesQuery.${system})
+          legacyPackages.${system});
 
-        # Used for nixpkgs packages, also accessible via `nix build`:
-        #
-        #     $ nix build .#<name>
-        #
-        inherit legacyPackages;
+      devPackages = eachSystem (system:
+        nixpkgs.lib.getAttrs
+          (nixpkgs.lib.attrNames devPackagesQuery)
+          legacyPackages.${system});
+    in
+    {
+      # Exposed packages that can be built or run with `nix build` or
+      # `nix run` respectively:
+      #
+      #     $ nix build .#<name>
+      #     $ nix run .#<name>
+      #     $ nix run .#<name> -- <args>
+      #
+      inherit packages;
 
-        # Development shells
-        #
-        #    $ nix develop .#<name>
-        #    $ nix develop .#<name> --command dune build @test
-        #
-        # [Direnv](https://direnv.net/) is recommended for automatically loading
-        # development environments in your shell. For example:
-        #
-        #    $ echo "use flake" > .envrc && direnv allow
-        #    $ dune build @test
-        #
-        devShells.default = pkgs.mkShell {
-          inputsFrom = builtins.attrValues testPackages;
-          buildInputs = builtins.attrValues devPackages ++ [
-            # You can add packages from nixpkgs here
-          ];
-        };
-      });
+      # Used for nixpkgs packages, also accessible via `nix build`:
+      #
+      #     $ nix build .#<name>
+      #
+      inherit legacyPackages;
+
+      # Development shells
+      #
+      #    $ nix develop .#<name>
+      #    $ nix develop .#<name> --command dune build @test
+      #
+      # [Direnv](https://direnv.net/) is recommended for automatically loading
+      # development environments in your shell. For example:
+      #
+      #    $ echo "use flake" > .envrc && direnv allow
+      #    $ dune build @test
+      #
+      devShells = eachSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+
+          # Packages with development dependencies enabled
+          packages =
+            nixpkgs.lib.getAttrs
+              (nixpkgs.lib.attrNames localPackagesQuery.${system})
+              (buildOpamProject system {
+                resolveArgs.dev = true;
+                resolveArgs.with-doc = true;
+                resolveArgs.with-test = true;
+              });
+        in
+        {
+          default = pkgs.mkShell {
+            inputsFrom = nixpkgs.lib.attrValues packages;
+            buildInputs = nixpkgs.lib.attrValues devPackages.${system} ++ [
+              # Packages from NixPkgs can be added here
+              pkgs.nixpkgs-fmt
+            ];
+          };
+        });
+    };
 }
