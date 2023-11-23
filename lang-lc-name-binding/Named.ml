7b4c121c2a8f3b24cd574b13ee1024f3a7d7e7a2
brendanzab <bjzaba@yahoo.com.au>
2023-11-24 02:05:49 +1100

    lang-lc-name-binding: Fix capture-avoidance
    Use naive capture avoiding substitution in `Named` interpreter

--- lang-lc-name-binding/Named.ml
+++ lang-lc-name-binding/Named.ml
@@ -1,5 +1,7 @@
 (** The lambda calculus, implemented using names. *)
 
+module StringSet = Set.Make (String)
+
 (** {1 Syntax} *)
 
 type expr =
@@ -37,14 +39,52 @@ let alpha_equiv (e1 : expr) (e2 : expr) =
   in
   go 0 ([], e1) ([], e2)
 
+(** {2 Free Variables} *)
+
+let rec fresh (ns : StringSet.t) (x : string) : string =
+  match StringSet.mem x ns with
+  | true -> fresh ns (x ^ "'")
+  | false -> x
+
+let rec free_vars (e : expr) : StringSet.t =
+  match e with
+  | Var x -> StringSet.singleton x
+  | Let (x, def, body) -> StringSet.(union (free_vars def) (remove x (free_vars body)))
+  | FunLit (x, body) -> StringSet.remove x (free_vars body)
+  | FunApp (head, arg) -> StringSet.union (free_vars head) (free_vars arg)
+
+let rec all_vars (e : expr) : StringSet.t =
+  match e with
+  | Var x -> StringSet.singleton x
+  | Let (_, def, body) -> StringSet.union (all_vars def) (all_vars body)
+  | FunLit (_, body) -> all_vars body
+  | FunApp (head, arg) -> StringSet.union (all_vars head) (all_vars arg)
+
 (** {2 Substitution} *)
 
+(** Capture avoiding substitution *)
 let rec subst (x, s : string * expr) (e : expr) : expr =
-  match e with
-  | Var y -> if x = y then s else e
-  | Let (y, def, body) -> if x = y then e else Let (y, def, subst (x, s) body)
-  | FunLit (y, body) -> if x = y then e else FunLit (y, subst (x, s) body)
-  | FunApp (head, arg) -> FunApp (subst (x, s) head, subst (x, s) arg)
+  (* Based on https://github.com/sweirich/lambda-n-ways/blob/c4ffc2add78d63b89c3d5d872f6787dadb890626/lib/Named/Lennart.hs#L83-L114 *)
+  let fvs = free_vars s in
+  let vs = StringSet.add x fvs in
+  let rec go e =
+    match e with
+    | Var y -> if x = y then s else e
+    | Let (y, _, _) when x = y -> e
+    | Let (y, def, body) when StringSet.mem y fvs ->
+        let y' = fresh (StringSet.union vs (all_vars body)) y in
+        let body' = subst (y, Var y') body in
+        Let (y', go def, go body')
+    | Let (y, def, body) -> Let (y, def, go body)
+    | FunLit (y, _) when x = y -> e
+    | FunLit (y, body) when StringSet.mem y fvs ->
+        let y' = fresh (StringSet.union vs (all_vars body)) y in
+        let body' = subst (y, Var y') body in
+        FunLit (y', go body')
+    | FunLit (y, body) -> FunLit (y, go body)
+    | FunApp (head, arg) -> FunApp (go head, go arg)
+  in
+  go e
 
 
 (** {1 Semantics} *)
