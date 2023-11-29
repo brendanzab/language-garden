5fb3d9fed7ed8903b78d092c3632dc27ce40b2f6
brendanzab <bjzaba@yahoo.com.au>
2023-11-29 11:02:29 +1100

    lang-lc-interpreters: Add `Lang` module

--- lang-lc-interpreters/Main.ml
+++ lang-lc-interpreters/Main.ml
@@ -5,14 +5,61 @@
 module Lexer = Lexer
 module Parser = Parser
 
-module Named = Named
-module NamedClosures = NamedClosures
-module NamedHoas = NamedHoas
+module type Lang = sig
 
-module Nameless = Nameless
-module NamelessClosures = NamelessClosures
-module NamelessHoas = NamelessHoas
+  type expr
 
-module Unique = Unique
-module UniqueClosures = UniqueClosures
-module UniqueHoas = UniqueHoas
+  val to_named : expr -> Named.expr
+  val of_named : Named.expr -> expr
+  val alpha_equiv : expr -> expr -> bool
+  val normalise : expr -> expr
+
+end
+[@@warning "-unused-value-declaration"]
+
+
+module Named : Lang with type expr = Named.expr = struct
+  include Named
+  let to_named = Fun.id
+  let of_named = Fun.id
+end
+
+module NamedClosures : Lang with type expr = Named.expr = struct
+  include NamedClosures
+  let to_named = Fun.id
+  let of_named = Fun.id
+  let normalise = normalise []
+end
+
+module NamedHoas : Lang with type expr = Named.expr = struct
+  include NamedHoas
+  let to_named = Fun.id
+  let of_named = Fun.id
+  let normalise = normalise []
+end
+
+
+module Nameless : Lang = Nameless
+
+module NamelessClosures : Lang = struct
+  include NamelessClosures
+  let normalise = normalise []
+end
+
+module NamelessHoas : Lang = struct
+  include NamelessHoas
+  let normalise = normalise []
+end
+
+
+module Unique : Lang = Unique
+
+module UniqueClosures : Lang = struct
+  include UniqueClosures
+  let normalise = normalise Id.Map.empty
+end
+
+module UniqueHoas : Lang = struct
+  include UniqueHoas
+  let normalise = normalise Id.Map.empty
+end
