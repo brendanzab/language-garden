2a2bfbed59dc534f5247e6519e7ff139bbdc6856
brendanzab <bjzaba@yahoo.com.au>
2024-02-12 10:37:13 +1100

    elab-letrec-unification: Initial commit

---
+++ elab-stlc-letrec-unification/Main.ml
@@ -0,0 +1,86 @@
+(** {0 Elaborator CLI} *)
+
+(** {1 Helper functions} *)
+
+let print_error (start, _ : Surface.loc) message =
+  Printf.eprintf "%s:%d:%d: %s\n"
+    start.pos_fname
+    start.pos_lnum
+    (start.pos_cnum - start.pos_bol)
+    message
+
+let parse_tm (filename : string) (input : in_channel) : Surface.tm =
+  let lexbuf = Sedlexing.Utf8.from_channel input in
+  Sedlexing.set_filename lexbuf filename;
+
+  try
+    lexbuf
+    |> Sedlexing.with_tokenizer Lexer.token
+    |> MenhirLib.Convert.Simplified.traditional2revised Parser.main
+  with
+  | Lexer.Error error ->
+      let msg =
+        match error with
+        | `UnexpectedChar -> "unexpected character"
+        | `UnclosedBlockComment -> "unclosed block comment"
+      in
+      print_error (Sedlexing.lexing_positions lexbuf) msg;
+      exit 1
+  | Parser.Error ->
+      print_error (Sedlexing.lexing_positions lexbuf) "syntax error";
+      exit 1
+
+let elab_tm (tm : Surface.tm) : Core.tm * Core.ty =
+  let tm, ty =
+    try Surface.elab_infer [] tm with
+    | Surface.Error (pos, msg) ->
+        print_error pos msg;
+        exit 1
+  in
+
+  match Surface.unsolved_metas () with
+  | [] ->
+      Core.zonk_tm tm,
+      Core.zonk_ty ty
+  | unsolved_metas ->
+      unsolved_metas |> List.iter (function
+        | (pos, `FunParam) -> print_error pos "ambiguous function parameter type"
+        | (pos, `FunBody) -> print_error pos "ambiguous function return type"
+        | (pos, `IfBranches) -> print_error pos "ambiguous if expression branches"
+        | (pos, `Placeholder) -> print_error pos "unsolved placeholder");
+      exit 1
+
+
+(** {1 Subcommands} *)
+
+let elab_cmd () : unit =
+  let tm, ty = elab_tm (parse_tm "<input>" stdin) in
+  Format.printf "@[<2>@[%a@ :@]@ @[%a@]@]@."
+    (Core.pp_tm []) tm
+    Core.pp_ty ty
+
+let norm_cmd () : unit =
+  let tm, ty = elab_tm (parse_tm "<input>" stdin) in
+  Format.printf "@[<2>@[%a@ :@]@ @[%a@]@]@."
+    (Core.pp_tm []) (Core.Semantics.normalise [] tm)
+    Core.pp_ty ty
+
+
+(** {1 CLI options} *)
+
+let cmd =
+  let open Cmdliner in
+
+  Cmd.group (Cmd.info "stlc-letrec-unification") [
+    Cmd.v (Cmd.info "elab" ~doc:"elaborate a term from standard input")
+      Term.(const elab_cmd $ const ());
+    Cmd.v (Cmd.info "norm" ~doc:"elaborate and normalise a term from standard input")
+      Term.(const norm_cmd $ const ());
+  ]
+
+
+(** {1 Main entrypoint} *)
+
+let () =
+  Printexc.record_backtrace true;
+  exit (Cmdliner.Cmd.eval cmd)
---
+++ elab-stlc-letrec-unification/README.md
@@ -0,0 +1,47 @@
+# Simply typed lambda calculus with unification
+
+This an elaborator for a simply typed lambda calculus (with booleans and integers)
+that allows programmers to omit type annotations. This is done by inserting
+_metavariables_ that stand-in for unknown types during elaboration. These are
+later updated based on how they are used in other parts of the program.
+
+This approach is a stepping-stone to more powerful type checking algorithms,
+such as those for Hindley-Milner type systems. Note that it’s not a highly
+optimised implementation – the goal here is clarity.
+
+This implementation was originally based on [Arad Arbel’s gist](https://gist.github.com/aradarbel10/837aa65d2f06ac6710c6fbe479909b4c).
+
+## Project overview
+
+| Module        | Description                             |
+| ------------- | --------------------------------------- |
+| [`Main`]      | Command line interface                  |
+| [`Lexer`]     | Lexer for the surface language          |
+| [`Parser`]    | Parser for the surface language         |
+| [`Surface`]   | Surface language, including elaboration |
+| [`Core`]      | Core language, including normalisation, unification, and pretty printing |
+
+[`Main`]: ./Main.ml
+[`Lexer`]: ./Lexer.mll
+[`Parser`]: ./Parser.mly
+[`Surface`]: ./Surface.ml
+[`Core`]: ./Core.ml
+
+## Examples
+
+```sh
+$ stlc-letrec-unification elab <<< "fun x => x + 2"
+fun (x : Int) => x + 2 : Int -> Int
+```
+
+```sh
+$ stlc-letrec-unification elab <<< "fun x f => f x * x"
+fun (x : Int) => fun (f : Int -> Int) => f x * x : Int -> (Int -> Int) -> Int
+```
+
+```sh
+$ stlc-letrec-unification elab <<< "fun x y => if x = 0 then y else 3"
+fun (x : Int) => fun (y : Int) => if x = 0 then y else 3 : Int -> Int -> Int
+```
+
+More examples can be found in [`tests.t`](tests.t).
