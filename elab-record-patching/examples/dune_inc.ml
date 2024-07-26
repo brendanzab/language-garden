1d91bf61f095b4e84827dd5f900cbdaa10e566bb
brendanzab <bjzaba@yahoo.com.au>
2024-07-26 13:51:42 +1000

    treewide: Dynamically generate dune rules

---
+++ elab-record-patching/examples/dune_inc.ml
@@ -0,0 +1,25 @@
+let bin = "record-patching"
+let package = "elab-record-patching"
+
+let generate_rules base =
+  Printf.printf
+    {|
+      (rule
+       (with-stdin-from ../%s.txt
+        (with-stdout-to %s.stdout.tmp
+         (run %%{bin:%s} elab))))
+
+      (rule
+       (alias runtest)
+       (package %s)
+       (action
+        (diff ../%s.stdout %s.stdout.tmp)))
+    |}
+    base base bin package base base
+
+let () =
+  Sys.readdir ".."
+  |> Array.to_list
+  |> List.sort String.compare
+  |> List.filter_map (Filename.chop_suffix_opt ~suffix:".txt")
+  |> List.iter generate_rules
---
+++ elab-stlc-bidirectional-stratify/examples/dune_inc.ml
@@ -0,0 +1,25 @@
+let bin = "stlc-bidirectional-stratify"
+let package = "elab-stlc-bidirectional-stratify"
+
+let generate_rules base =
+  Printf.printf
+    {|
+      (rule
+       (with-stdin-from ../%s.txt
+        (with-stdout-to %s.stdout.tmp
+         (run %%{bin:%s} elab))))
+
+      (rule
+       (alias runtest)
+       (package %s)
+       (action
+        (diff ../%s.stdout %s.stdout.tmp)))
+    |}
+    base base bin package base base
+
+let () =
+  Sys.readdir ".."
+  |> Array.to_list
+  |> List.sort String.compare
+  |> List.filter_map (Filename.chop_suffix_opt ~suffix:".txt")
+  |> List.iter generate_rules
