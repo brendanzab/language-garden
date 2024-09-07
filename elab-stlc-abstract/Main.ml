e3a2f3eaccec78fe1049e0c715a732b7542d9907
brendanzab <bjzaba@yahoo.com.au>
2024-09-07 13:02:25 +1000

    elab-stlc-abstract: Simplify entrypoint

--- elab-stlc-abstract/Main.ml
+++ elab-stlc-abstract/Main.ml
@@ -1,7 +1,5 @@
 (** {0 Elaborator CLI} *)
 
-(** {1 Helper functions} *)
-
 let print_error (severity : string) (start, _ : Lexing.position * Lexing.position) (message : string) =
   Printf.eprintf "%s:%d:%d: %s: %s\n"
     start.pos_fname
@@ -10,16 +8,26 @@ let print_error (severity : string) (start, _ : Lexing.position * Lexing.positio
     severity
     message
 
-let parse_tm (filename : string) (input : in_channel) : Surface.tm =
-  let lexbuf = Sedlexing.Utf8.from_channel input in
-  Sedlexing.set_filename lexbuf filename;
 
-  try
+(** {1 Main entrypoint} *)
+
+let () =
+  Printexc.record_backtrace true;
+
+  let lexbuf = Sedlexing.Utf8.from_channel stdin in
+  Sedlexing.set_filename lexbuf "<input>";
+
+  match
     lexbuf
     |> Sedlexing.with_tokenizer Lexer.token
     |> MenhirLib.Convert.Simplified.traditional2revised Parser.main
+    |> Surface.elab_synth
   with
-  | Lexer.Error error ->
+  | tm, ty ->
+      Format.printf "@[<2>@[%a@ :@]@ @[%a@]@]@."
+        Core.pp_tm tm
+        Core.pp_ty ty
+  | exception Lexer.Error error ->
       let msg =
         match error with
         | `UnexpectedChar -> "unexpected character"
@@ -27,28 +35,12 @@ let parse_tm (filename : string) (input : in_channel) : Surface.tm =
       in
       print_error "error" (Sedlexing.lexing_positions lexbuf) msg;
       exit 1
-  | Parser.Error ->
+  | exception Parser.Error ->
       print_error "error" (Sedlexing.lexing_positions lexbuf) "syntax error";
       exit 1
-
-let elab_tm (tm : Surface.tm) : Core.tm * Core.ty =
-  try Surface.elab_synth tm with
-  | Surface.Error (loc, msg) ->
+  | exception Surface.Error (loc, msg) ->
       print_error "error" loc msg;
       exit 1
-  | Surface.Bug (loc, msg) ->
+  | exception Surface.Bug (loc, msg) ->
       print_error "bug" loc msg;
       exit 1
-
-
-(** {1 Main entrypoint} *)
-
-let () =
-  Printexc.record_backtrace true;
-
-  let tm = parse_tm "<input>" stdin in
-  let tm, ty = elab_tm tm in
-
-  Format.printf "@[<2>@[%a@ :@]@ @[%a@]@]@."
-    Core.pp_tm tm
-    Core.pp_ty ty
