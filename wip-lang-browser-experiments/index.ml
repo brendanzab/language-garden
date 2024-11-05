23e662793222c20b9c0298b17688da2d4b1d6c52
brendanzab <bjzaba@yahoo.com.au>
2024-11-05 22:21:15 +1100

    wip-lang-browser-experiments: Clean up canvas initialisation

--- wip-lang-browser-experiments/index.ml
+++ wip-lang-browser-experiments/index.ml
@@ -2,10 +2,12 @@ module Dom = Js_of_ocaml.Dom
 module Html = Js_of_ocaml.Dom_html
 module Js = Js_of_ocaml.Js
 
-let get_canvas_by_id (id : string) =
-  Js.Opt.get
-    (Html.getElementById id |> Html.CoerceTo.canvas)
-    (fun () -> failwith "expected canvas")
+let canvas_width = 400.0
+let canvas_height = 400.0
+
+let apex_diameter = 3.0
+let branch_len = 6.0
+let fork_angle = 45.0
 
 let init_tree (iters : int) : Binary_tree.tree =
   let rec go iters tree =
@@ -14,9 +16,6 @@ let init_tree (iters : int) : Binary_tree.tree =
   in
   go iters Binary_tree.axiom
 
-let branch_len = 6.0
-let fork_angle = 45.0
-
 let rec draw (ctx : Html.canvasRenderingContext2D Js.t) (tree : Binary_tree.tree) : unit =
   match tree with
   | Apex ->
@@ -24,7 +23,7 @@ let rec draw (ctx : Html.canvasRenderingContext2D Js.t) (tree : Binary_tree.tree
         ctx##arc
           (* center_x *) (Js.float 0.0)
           (* center_y *) (Js.float 0.0)
-          (* radius *) (Js.float 1.5)
+          (* radius *) (Js.float (apex_diameter *. 0.5))
           (* theta1 *) (Js.float 0.0)
           (* theta2 *) (Js.float (2.0 *. Float.pi))
           (* ccw *) Js._false;
@@ -60,21 +59,19 @@ let rec draw (ctx : Html.canvasRenderingContext2D Js.t) (tree : Binary_tree.tree
       end
 
 let start (_ : (#Html.event as 'b) Js.t) : bool Js.t = begin
-  let width = 400.0 in
-  let height = 400.0 in
-
   (* Initialise canvas and 2D drawing context *)
-  let canvas = get_canvas_by_id "tree-canvas" in
+  let canvas = Html.createCanvas Html.document in
   let ctx = canvas##getContext Html._2d_ in
+  Dom.appendChild (Html.getElementById "tree") canvas;
 
   (* Set display size in CSS pixels *)
-  canvas##.style##.width := Js.string (Format.sprintf "%.0fpx" width);
-  canvas##.style##.height := Js.string (Format.sprintf "%.0fpx" height);
+  canvas##.style##.width := Js.string (Format.sprintf "%.0fpx" canvas_width);
+  canvas##.style##.height := Js.string (Format.sprintf "%.0fpx" canvas_height);
 
   (* Set the actual size in memory *)
   let scale = Html.window##.devicePixelRatio in
-  canvas##.width := int_of_float (width *. scale);
-  canvas##.height := int_of_float (height *. scale);
+  canvas##.width := int_of_float (canvas_width *. scale);
+  canvas##.height := int_of_float (canvas_height *. scale);
 
   (* Normalise the coordinate system to CSS pixels *)
   ctx##scale scale scale;
@@ -83,7 +80,7 @@ let start (_ : (#Html.event as 'b) Js.t) : bool Js.t = begin
   begin
     ctx##save;
 
-    ctx##translate (width /. 2.0) height;
+    ctx##translate (canvas_width *. 0.5) canvas_height;
     draw ctx (init_tree 5);
 
     ctx##restore;
