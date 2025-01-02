586cc983e6529d1f630aa5609ab6a7813603f317
brendanzab <bjzaba@yahoo.com.au>
2025-01-03 09:53:38 +1100

    lang-declarative-graphics: Update tree example
    Make it consistent with thbinary tree example in the
    `lang-fractal-tree-rewriting` project.

--- lang-declarative-graphics/examples/tree.ml
+++ lang-declarative-graphics/examples/tree.ml
@@ -1,29 +1,35 @@
 open Declarative_graphics_core
 
-let tree (type t) (module D : Diagram.S with type t = t) ~(theta : float) ~(width:float) ~(height:float) : t =
-  let rec branch (len : float) (n : int) : t =
+let apex_diameter = 3.0
+let fork_angle = 45.0 *. Float.pi /. 180.0
+let branch_length = 6.0
+
+let tree (type t) (module D : Diagram.S with type t = t) ~(iters : int) : t =
+  let rec  apex =
+    D.circle ~diameter:apex_diameter
+
+  and branch ~(iters : int) : t =
+    let len = branch_length *. Float.pow 2.0 (float_of_int iters) in
     D.stack [
       D.line (0.0, 0.0) (0.0, -.len)
         |> D.stroke `solid;
       D.translate_y (-.len)
-        (if n <= 0 then apex else fork (len *. 0.66) (n - 1))
+        (if iters <= 0 then apex else fork ~iters);
     ]
-  and apex =
-    D.circle ~diameter:3.0
-  and fork (len : float) (n : int) =
+
+  and fork ~(iters : int) =
     D.stack [
-      branch len n |> D.rotate ~radians:(+.theta);
-      branch len n |> D.rotate ~radians:(-.theta);
+      branch ~iters:(iters - 1) |> D.rotate ~radians:(+.fork_angle);
+      branch ~iters:(iters - 1) |> D.rotate ~radians:(-.fork_angle);
     ]
   in
 
-  branch 150.0 4
-    |> D.translate (width /. 2.0, height)
+  branch ~iters
 
 let () =
-  let theta = Float.pi /. 6.0 in
-  let (width, height) = (800.0, 500.0) in
+  let (width, height) = (400.0, 400.0) in
 
-  tree (module Svg_diagram) ~theta ~width ~height
+  tree (module Svg_diagram) ~iters:5
+    |> Svg_diagram.translate (width /. 2.0, height)
     |> Svg_diagram.run ~view_box:(0.0, 0.0, width, height)
     |> print_string
