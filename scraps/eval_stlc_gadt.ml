09194df2bc322316ec672ab17740a56044bf673c
brendanzab <bjzaba@yahoo.com.au>
2025-04-22 13:46:50 +1000

    scraps: Add environment type

--- scraps/eval_stlc_gadt.ml
+++ scraps/eval_stlc_gadt.ml
@@ -1,5 +1,9 @@
 (** A well-typed lambda calculus evaluator using GADTs. *)
 
+type 'ctx env =
+  | [] : unit env
+  | ( :: ) : 'a * 'ctx env -> ('a * 'ctx) env
+
 type ('ctx, 'a) var =
   | Stop : ('a * 'ctx, 'a) var
   | Pop : ('ctx, 'a) var -> ('b * 'ctx, 'a) var
@@ -10,22 +14,22 @@ type ('ctx, 'a) expr =
   | Fun_abs : ('a * 'ctx, 'b) expr -> ('ctx, 'a -> 'b) expr
   | Fun_app : ('ctx, 'a -> 'b) expr * ('ctx, 'a) expr -> ('ctx, 'b) expr
 
-let rec lookup : type ctx a. (ctx, a) var -> ctx -> a =
+let rec lookup : type ctx a. (ctx, a) var -> ctx env -> a =
   fun x env ->
     match x, env with
-    | Stop, (v, _) -> v
-    | Pop x, (_, env) -> lookup x env
+    | Stop, v :: _ -> v
+    | Pop x, _ :: env -> lookup x env
 
-let rec eval : type ctx a. ctx -> (ctx, a) expr -> a =
+let rec eval : type ctx a. ctx env -> (ctx, a) expr -> a =
   fun env expr ->
     match expr with
-    | Let (def, body) -> eval (eval env def, env) body
+    | Let (def, body) -> eval (eval env def :: env) body
     | Var x -> lookup x env
-    | Fun_abs body -> fun x -> eval (x, env) body
+    | Fun_abs body -> fun x -> eval (x :: env) body
     | Fun_app (fn, arg) -> (eval env fn) (eval env arg)
 
 let () =
-  assert (eval () (Fun_abs (Var Stop)) 1 = 1);
-  assert (eval () (Fun_abs (Fun_abs (Var (Pop Stop)))) "hello" 4 = "hello");
-  assert (eval ("hello", ()) (Fun_app (Fun_abs (Fun_abs (Var (Pop Stop))), Var Stop)) 4 = "hello");
-  assert (eval (2, ("hello", ())) (Let (Var (Pop Stop), Var Stop)) = "hello");
+  assert (eval [] (Fun_abs (Var Stop)) 1 = 1);
+  assert (eval [] (Fun_abs (Fun_abs (Var (Pop Stop)))) "hello" 4 = "hello");
+  assert (eval ["hello"] (Fun_app (Fun_abs (Fun_abs (Var (Pop Stop))), Var Stop)) 4 = "hello");
+  assert (eval [2; "hello"] (Let (Var (Pop Stop), Var Stop)) = "hello");
