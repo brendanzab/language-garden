455ec019183abaae09ef8ae7be1325a7dc2346a8
brendanzab <bjzaba@yahoo.com.au>
2025-05-28 18:45:24 +1000

    elab-system-f-unification: Improve docs and parameter order

--- elab-system-f-unification/core.ml
+++ elab-system-f-unification/core.ml
@@ -320,7 +320,22 @@ exception Mismatched_types of Semantics.vty * Semantics.vty
 exception Infinite_type of Meta.t
 exception Escaping_scope of Meta.t
 
-let validate_meta_solution (ty_size : int) (m : Meta.t) (ty_level : level) (vty : Semantics.vty) =
+(** Validate an unsolved metavariable against a solution candidate type:
+
+    - Occurs check:
+        This ensures that the candidate type does not refer to the
+        to-be-solved metavariable (this would result in infinite loops).
+
+    - Scope check:
+        This ensures that the candidate type does not refer to local type
+        variables that are “to-the-right” of to-be-solved metavariable in
+        the typing context.
+
+    - Level raising:
+        This raises the levels of metavariables in the candidate type
+        to match the level of the to-be-solved metavariable.
+*)
+let validate_meta_solution (m, ty_level : Meta.t * level) (ty_size, vty : int * Semantics.vty) =
   (** Traverse the solution candidate type, using the size of the typing
       context to generate free variables under binders. *)
   let rec go ty_size' (vty : Semantics.vty) =
@@ -333,19 +348,24 @@ let validate_meta_solution (ty_size : int) (m : Meta.t) (ty_level : level) (vty 
     | Int_type -> ()
     | Bool_type -> ()
     | Local_var ty_level' ->
-        (* Throw an error if a to-be-solved metavariable would depend on type
-            variables to “the right” of it in the context. *)
+        (* Scope check *)
         if ty_level <= ty_level' && ty_level' < ty_size then
+          (* Throw an error if a to-be-solved metavariable would depend on type
+              variables to “the right” of it in the context. *)
           raise (Escaping_scope m)
     | Meta_var m' ->
-        if m == m' then raise (Infinite_type m);
+        (* Occurs check *)
+        if m == m' then
+          raise (Infinite_type m);
+        (* Level raising *)
         begin match !m' with
         | Unsolved { id = id'; ty_level = ty_level' } ->
             (* Raise the level constraint on metavariables to match the level
                of the to-be-solved metavariable. *)
             if ty_level < ty_level' then
               m' := Unsolved { id = id'; ty_level }
-        | Solved vty -> go ty_size' vty
+        | Solved vty ->
+            go ty_size' vty
         end
   in
   go ty_size vty
@@ -359,7 +379,7 @@ let rec unify_vtys (ty_size : int) (vty1 : Semantics.vty) (vty2 : Semantics.vty)
       begin match !m with
       | Solved vty' -> unify_vtys ty_size vty vty'
       | Unsolved { ty_level; _ } ->
-          validate_meta_solution ty_size m ty_level vty;
+          validate_meta_solution (m, ty_level) (ty_size, vty);
           m := Solved vty
       end
   | Forall_type (_, body_ty1), Forall_type (_, body_ty2) ->
