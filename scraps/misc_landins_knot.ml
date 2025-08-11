30f612b33358b698014870aa42d27f5c30fb3217
brendanzab <bjzaba@yahoo.com.au>
2025-08-11 11:49:56 +1000

    change(scraps): Update Landin’s knot example

--- scraps/misc_landins_knot.ml
+++ scraps/misc_landins_knot.ml
@@ -4,26 +4,40 @@
     {{: https://doi.org/10.1093/comjnl/6.4.308} The Mechanical Evaluation of Expressions}
 *)
 
-[@@@warning "-unused-value-declaration"]
-
-let knot (type a b) (f : (a -> b) -> a -> b) : a -> b =
+let fix (type a b) (f : (a -> b) -> a -> b) : a -> b =
   let recur = ref (fun _ -> assert false) in  (* create a reference to be used inside the recursive function *)
-  let fix = f (fun x -> !recur x) in          (* create the recursive function *)
-  recur := fix;                               (* backpatch the recursive function to call itself *)
-  fix                                         (* return the recursive function *)
+  let knot = f (fun x -> !recur x) in         (* create the recursive function *)
+  recur := knot;                              (* backpatch the recursive function to call itself *)
+  knot                                        (* return the recursive function *)
 
 let fact : int -> int =
-  knot @@ fun fact x ->
+  fix @@ fun fact x ->
     match x with
     | 0 -> 1
     | x -> x * fact (x - 1)
 
 let length (type a) : a list -> int =
-  knot @@ fun size xs ->
+  fix @@ fun length xs ->
     match xs with
     | [] -> 0
-    | _ :: xs -> 1 + size xs
+    | _ :: xs -> 1 + length xs
 
-let () =
-  Seq.ints 0 |> Seq.take 10 |> Seq.iter @@ fun x ->
-    Printf.printf "fact %i = %i\n" x (fact x)
+
+let () = begin
+
+  assert (fact 0 = 1);
+  assert (fact 1 = 1);
+  assert (fact 2 = 2);
+  assert (fact 3 = 6);
+  assert (fact 4 = 24);
+  assert (fact 5 = 120);
+  assert (fact 6 = 720);
+  assert (fact 7 = 5040);
+  assert (fact 8 = 40320);
+  assert (fact 9 = 362880);
+
+  assert (length [] = 0);
+  assert (length [1] = 1);
+  assert (length [1; 2; 3; 4; 5] = 5);
+
+end
