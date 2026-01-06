dcd57846fbf8383a1cc6e663e4182a0b80126935
brendanzab <bjzaba@yahoo.com.au>
2026-01-06 21:44:38 +1100

    Simplify modules

--- scraps/compile_lc_machines.ml
+++ scraps/compile_lc_machines.ml
@@ -26,133 +26,114 @@ let rec step_many : type state. (module Machine with type state = state) -> stat
   | Some state -> step_many (module M) state
   | None -> state
 
-(** Constants *)
-module type Const = sig
 
-  type t
+(** Constants *)
+module Const = struct
+
+  type t =
+    | Int of int
+    | String of string
 
 end
+
+
+(** {2 Abstract Machines} *)
 
 (** Untyped lambda calculus expressions *)
 module Expr = struct
 
-  module Make (C : Const) = struct
+  (** De-bruijn index *)
+  type index = int
 
-    (** De-bruijn index *)
-    type index = int
-
-    type t =
-      | Var of index
-      | Let of string * t * t
-      | Fun_intro of string * t
-      | Fun_app of t * t
-      | Const of C.t
-
-  end
+  type t =
+    | Var of index
+    | Let of string * t * t
+    | Fun_intro of string * t
+    | Fun_app of t * t
+    | Const of Const.t
 
 end
 
 (** Landin’s SECD Machine (Call-by-value) *)
 module Secd = struct
 
-  module Data (C : Const) = struct
+  (** De-bruijn index *)
+  type index = int
 
-    (** De-bruijn index *)
-    type index = int
+  (** Machine instructions *)
+  type instr =
+    | Var of index        (* Load a variable from the environment and push it to the stack *)
+    | Let_def             (* Pop a value and add it to the environment *)
+    | Let_end             (* Discard the first entry in the environment *)
+    | Fun_intro of prog   (* Push a closure to the stack *)
+    | Fun_app             (* Begin executing a closure with an argument, saving the calling frame *)
+    | Fun_end             (* Terminate the function and resume the rest of the program *)
+    | Const of Const.t    (* Push a constant to the stack *)
 
-    (** Machine instructions *)
-    type instr =
-      | Var of index        (* Load a variable from the environment and push it to the stack *)
-      | Let_def             (* Pop a value and add it to the environment *)
-      | Let_end             (* Discard the first entry in the environment *)
-      | Fun_intro of prog   (* Push a closure to the stack *)
-      | Fun_app             (* Begin executing a closure with an argument, saving the calling frame *)
-      | Fun_end             (* Terminate the function and resume the rest of the program *)
-      | Const of C.t        (* Push a constant to the stack *)
+  and value =
+    | Clos of prog * env
+    | Const of Const.t
 
-    and value =
-      | Clos of prog * env
-      | Const of C.t
+  (** Call-frames, for resuming a computation later *)
+  and frame = prog * env * stack
 
-    (** Call-frames, for resuming a computation later *)
-    and frame = prog * env * stack
+  and stack = value list        (* Return (S)tack *)
+  and env = value list          (* (E)nvironment *)
+  and prog = instr list         (* (C)ontrol stack *)
+  and frames = frame list       (* (D)ump stack *)
 
-    and stack = value list        (* Return (S)tack *)
-    and env = value list          (* (E)nvironment *)
-    and prog = instr list         (* (C)ontrol stack *)
-    and frames = frame list       (* (D)ump stack *)
+  (** State of the abstract machine *)
+  type state = prog * env * stack * frames
 
-    (** State of the abstract machine *)
-    type state = prog * env * stack * frames
+  (** Compile lambda calculus expressions into a list of SECD instructions *)
+  let compile_expr (expr : Expr.t) : prog =
+    let ( << ) = Fun.compose in
+    let rec compile (expr : Expr.t) : prog -> prog =
+      match expr with
+      | Expr.Var i -> List.cons (Var i : instr)
+      | Expr.Let (_, e1, e2) -> compile e1 << List.cons Let_def << compile e2 << List.cons Let_end
+      | Expr.Fun_intro (_, e) -> List.cons (Fun_intro (compile e [Fun_end]))
+      | Expr.Fun_app (e1, e2) -> compile e1 << compile e2 << List.cons Fun_app
+      | Expr.Const c -> List.cons (Const c : instr)
+    in
+    compile expr []
 
-  end
+  let inject (prog : prog) : state =
+    prog, [], [], []
 
-  module Make (C : Const) : sig
+  let step : state -> state option = function
+    (* Lookup a variable and push it to the stack *)
+    | Var i :: prog, env, stack, frames ->
+        List.nth_opt env i |> Option.map @@ fun v ->
+          prog, env, v :: stack, frames
 
-    include module type of Data (C)
+    (* Move a definition from the stack to the environment *)
+    | Let_def :: prog, env, v :: stack, frames ->
+        Some (prog, v :: env, stack, frames)
 
-    (** Compile lambda calculus expressions into a list of SECD instructions *)
-    val compile_expr : Expr.Make (C).t -> prog
+    (* Exiting a let binding, so pop the definition off the environment *)
+    | Let_end :: prog, _ :: env, stack, frames ->
+        Some (prog, env, stack, frames)
 
-    include Machine
-      with type prog := prog
-      with type state := state
+    (* Push a closure onto the stack *)
+    | Fun_intro prog' :: prog, env, stack, frames ->
+        Some (prog, env, Clos (prog', env) :: stack, frames)
 
-  end = struct
+    (* Suspend the calling frame, and begin executing the body of a closure
+        with argument bound in the environment. *)
+    | Fun_app :: prog, env, v :: Clos (prog', env') :: stack, frames ->
+        Some (prog', v :: env', [], (prog, env, stack) :: frames)
 
-    include Data (C)
+    (* Resume the calling frame, moving the value at the top of the stack
+        to the previously suspended stack *)
+    | Fun_end :: _, _, v :: _, (prog, env, stack) :: frames ->
+        Some (prog, env, v :: stack, frames)
 
-    let compile_expr (expr : Expr.Make (C).t) : prog =
-      let module Expr = Expr.Make (C) in
-      let ( << ) = Fun.compose in
-      let rec compile (expr : Expr.t) : prog -> prog =
-        match expr with
-        | Expr.Var i -> List.cons (Var i : instr)
-        | Expr.Let (_, e1, e2) -> compile e1 << List.cons Let_def << compile e2 << List.cons Let_end
-        | Expr.Fun_intro (_, e) -> List.cons (Fun_intro (compile e [Fun_end]))
-        | Expr.Fun_app (e1, e2) -> compile e1 << compile e2 << List.cons Fun_app
-        | Expr.Const c -> List.cons (Const c : instr)
-      in
-      compile expr []
+    (* Push a constant to the stack *)
+    | Const c :: prog, env, stack, frames ->
+        Some (prog, env, Const c :: stack, frames)
 
-    let inject (prog : prog) : state =
-      prog, [], [], []
-
-    let step : state -> state option = function
-      (* Lookup a variable and push it to the stack *)
-      | Var i :: prog, env, stack, frames ->
-          List.nth_opt env i |> Option.map @@ fun v ->
-            prog, env, v :: stack, frames
-
-      (* Move a definition from the stack to the environment *)
-      | Let_def :: prog, env, v :: stack, frames ->
-          Some (prog, v :: env, stack, frames)
-
-      (* Exiting a let binding, so pop the definition off the environment *)
-      | Let_end :: prog, _ :: env, stack, frames ->
-          Some (prog, env, stack, frames)
-
-      (* Push a closure onto the stack *)
-      | Fun_intro prog' :: prog, env, stack, frames ->
-          Some (prog, env, Clos (prog', env) :: stack, frames)
-
-      (* Suspend the calling frame, and begin executing the body of a closure
-         with argument bound in the environment. *)
-      | Fun_app :: prog, env, v :: Clos (prog', env') :: stack, frames ->
-          Some (prog', v :: env', [], (prog, env, stack) :: frames)
-
-      (* Resume the calling frame, moving the value at the top of the stack
-         to the previously suspended stack *)
-      | Fun_end :: _, _, v :: _, (prog, env, stack) :: frames ->
-          Some (prog, env, v :: stack, frames)
-
-      (* Push a constant to the stack *)
-      | Const c :: prog, env, stack, frames ->
-          Some (prog, env, Const c :: stack, frames)
-
-      | _ -> None
-
-  end
+    | _ -> None
 
 end
 
@@ -169,6 +150,8 @@ module Kirvine = struct (* TODO *) end
 module Zam = struct (* TODO *) end
 
 
+(* Tests *)
+
 let () = begin
 
   Printexc.record_backtrace true;
@@ -179,9 +162,6 @@ let () = begin
       | Int of int
 
   end in
-
-  let module Expr = Expr.Make (Const) in
-  let module Secd = Secd.Make (Const) in
 
   let ( $ ) f a = Expr.Fun_app (f, a) in
 
