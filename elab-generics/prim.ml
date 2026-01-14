a662828325f329e196ee5a0306376e68622aa9f5
brendanzab <bjzaba@yahoo.com.au>
2026-01-14 15:57:47 +1100

    Remove unused modules

--- elab-generics/prim.ml
+++ elab-generics/prim.ml
@@ -14,26 +14,3 @@ let name (prim : t) : string =
   | Int_sub -> "int-sub"
   | Int_mul -> "int-mul"
   | Int_neg -> "int-neg"
-
-
-module Test = struct
-
-  type _ prim_fun =
-    | Bool_eq : (bool -> bool -> bool) prim_fun
-    | Int_eq : (int -> int -> bool) prim_fun
-    | Int_add : (int -> int -> int) prim_fun
-    | Int_sub : (int -> int -> int) prim_fun
-    | Int_mul : (int -> int -> int) prim_fun
-    | Int_neg : (int -> int) prim_fun
-
-  let sem : type f. f prim_fun -> f = function
-    | Bool_eq -> Bool.equal
-    | Int_eq -> Int.equal
-    | Int_add -> Int.add
-    | Int_sub -> Int.sub
-    | Int_mul -> Int.mul
-    | Int_neg -> Int.neg
-
-  let _ : bool = sem Int_eq 2 3
-
-end
