9c3cc8813e494bcd73c8b32beff08b8a99141248
brendanzab <bjzaba@yahoo.com.au>
2026-03-05 17:59:38 +1100

    Add example of compiling nested actions to monadic bind

---
+++ scraps/compile_lc_nested_actions.ml
@@ -0,0 +1,184 @@
+(** An example of translating “nested actions” (a.k.a. “!-notation”) to monadic
+    bind operations.
+
+    Monadic code in languages like Haskell and OCaml requires the result of each
+    effectful computation to be bound to an intermediate result. This results in
+    verbose, unnatural code, not unlike the result of translating to A-normal
+    form. Idris and Lean on the other hand allow effectful code to be expressed
+    in a more direct style with “!-notation” and “nested actions” respectively.
+
+    For example:
+
+    {@text[
+      let x <- readInt;
+      let y <- readInt;
+      printInt (x + y)
+    ]}
+
+    Can be expressed more directly as:
+
+    {@text[
+      printInt (!readInt + !readInt)
+    ]}
+
+    {2 Todo}
+
+    - Idiom brackets
+    - Monad comprehensions
+
+    {2 Resources}
+
+    - https://docs.idris-lang.org/en/latest/tutorial/interfaces.html#notation
+    - https://lean-lang.org/doc/reference/latest/Functors___-Monads-and--do--Notation/Syntax
+*)
+
+(** Fresh variable names *)
+module Id : sig
+
+  type t
+
+  val fresh : unit -> t
+  val compare : t -> t -> int
+
+end = struct
+
+  type t = int
+
+  let next_id = ref 0
+
+  let fresh () : t =
+    let id = !next_id in
+    incr next_id;
+    id
+
+  let compare = Int.compare
+
+end
+
+(** The source language *)
+module L1 = struct
+
+  module Expr = struct
+
+    [@@@warning "-unused-constructor"]
+
+    type t =
+      | Var of Id.t                 (* x *)
+      | Let of Id.t * t * t         (* let x := e; e *)
+      | Eff_bind of Id.t * t * t    (* let x <- e; e *)
+      | Eff_pure of t               (* pure e *)
+      | Eff_action of t             (* !e *)
+      | Fun_lit of Id.t * t         (* \x. e *)
+      | Fun_app of t * t            (* e e *)
+
+  end
+
+end
+
+(** The target language *)
+module L2 = struct
+
+  module Expr = struct
+
+    type t =
+      | Var of Id.t                 (* x *)
+      | Let of Id.t * t * t         (* let x := e; e *)
+      | Eff_bind of Id.t * t * t    (* let x <- e; e *)
+      | Eff_pure of t               (* pure e *)
+      | Fun_lit of Id.t * t         (* \x. e *)
+      | Fun_app of t * t            (* e e *)
+
+  end
+
+end
+
+(** Monadic code looks quite similar to A-normal form! This suggests that we can
+    use similar approach to {{: https://matt.might.net/articles/a-normalization/}
+    ANF-translation} to turn nested actions in the source language into bind
+    operations in the target language.
+
+    Like in ANF-translation, our translation function takes a continuation that
+    represents “the rest of the expression” that we are currently translating.
+    This allows us to wrap the translated expression in a new monadic binding
+    whenever we hit a nested action.
+*)
+
+let ( let@ ) = ( @@ )
+
+let rec translate (expr : L1.Expr.t) (k : L2.Expr.t -> L2.Expr.t) : L2.Expr.t =
+  match expr with
+  | L1.Expr.Var x ->
+      k (L2.Expr.Var x)
+  | L1.Expr.Let (x, def, body) ->
+      let@ def = translate def in
+      L2.Expr.Let (x, def, translate body k)
+  | L1.Expr.Eff_bind (x, def, body) ->
+      let@ def = translate def in
+      L2.Expr.Eff_bind (x, def, translate body k)
+  | L1.Expr.Eff_pure expr ->
+      let@ expr = translate expr in
+      k (L2.Expr.Eff_pure expr)
+
+  (* Lift the expression as high as possible in the current scope, binding
+     it to a fresh name. *)
+  | L1.Expr.Eff_action expr ->
+      let x = Id.fresh () in
+      let@ expr = translate expr in
+      (* The “rest of the expression” will be nested inside the new binding *)
+      L2.Expr.Eff_bind (x, expr, k (Var x))
+
+  | L1.Expr.Fun_lit (x, body) ->
+      k (L2.Expr.Fun_lit (x, translate body Fun.id))
+  | L1.Expr.Fun_app (head, arg) ->
+      let@ head = translate head in
+      let@ arg = translate arg in
+      k (L2.Expr.Fun_app (head, arg))
+
+let translate (e : L1.Expr.t) : L2.Expr.t =
+  translate e Fun.id
+
+
+(* Example from https://docs.idris-lang.org/en/latest/tutorial/interfaces.html#notation *)
+let () =
+  let y = Id.fresh () in
+  let _42 = Id.fresh () in
+  let f = Id.fresh () in
+  let g = Id.fresh () in
+  let print = Id.fresh () in
+  let x = Id.fresh () in
+
+  (*
+    let y := 42;
+    f !(g !(print y) !x)
+  *)
+  let src = L1.Expr.(
+    let ( $ ) f x = Fun_app (f, x) in
+    let ( ! ) e = Eff_action e in
+    Let (y, Var _42, Var f $ !(Var g $ !(Var print $ Var y) $ !(Var x)))
+  ) in
+
+  (*
+    let y = 42;
+    let $y <- print y;
+    let $x <- x;
+    let $g <- g $y $x;
+    f $g
+  *)
+  match translate src with
+  | Let (y', Var _42',
+      Eff_bind (fresh_y, Fun_app (Var print', Var y''),
+        Eff_bind (fresh_x, Var x',
+          Eff_bind (fresh_g, Fun_app (Fun_app (Var g', Var fresh_y'), Var fresh_x'),
+            Fun_app (Var f', Var fresh_g'))))) ->
+      assert (Id.compare y y' = 0);
+      assert (Id.compare _42 _42' = 0);
+      assert (Id.compare print print' = 0);
+      assert (Id.compare y y'' = 0);
+      assert (Id.compare x x' = 0);
+      assert (Id.compare g g' = 0);
+      assert (Id.compare fresh_y fresh_y' = 0);
+      assert (Id.compare fresh_x fresh_x' = 0);
+      assert (Id.compare fresh_g fresh_g' = 0);
+      assert (Id.compare f f' = 0);
+  | _ ->
+      assert false
